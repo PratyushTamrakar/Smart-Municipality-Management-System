114e93a099a83147d5c6198d7076265bb82531fd
Ruben Bhattarai <rubenb@kfabusinessschool.edu.np>
2026-08-18 20:31:57 +0545

    commited

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 public interface UserDAO {
     boolean registerUser(User user);
     Optional<User> login(String email, String password);
+    Optional<User> getUserById(int userId); // Add this method signature
 }
